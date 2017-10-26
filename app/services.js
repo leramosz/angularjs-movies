d06f4ed680b5093a2a2ef6e7992a8c6958b481b3
Leonardo Ramos <Leo@Leonardos-MBP.iptv.microsoft.com>
2017-10-25 23:37:58 -0300

    app v5

--- app/services.js
+++ app/services.js
@@ -27,7 +27,6 @@ function DataService($http, ApiBasePath, $location) {
             }).then(function (response) {
                return response.data
             }).catch(function(error) {
-               //return error.data;
                $location.url('/404/');
             });
   };
